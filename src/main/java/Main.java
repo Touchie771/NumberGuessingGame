e93592543ecb4faec4ba0d00e8a4be3f59e209d9
Touchie771 <mihai.constantin.bara@gmail.com>
2025-10-24 18:49:45 +0300

    implement game loop

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,17 +4,23 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        int x = new Random().nextInt(0, 101);
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("---Number Guessing Game---\n");
-        System.out.println("x is a natural number and x < 101\n");
         while (true) {
-            System.out.println("Enter your guess: ");
-            int guess = scanner.nextInt();
-            if (guess < x) { System.out.println("x is greater!"); continue;}
-            if (guess > x) { System.out.println("x is smaller!"); continue;}
-            System.out.println("You were correct!");
-            break;
+            int x = new Random().nextInt(0, 101);
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("---Number Guessing Game---\n");
+            System.out.println("x is a natural number and x < 101\n");
+            while (true) {
+                System.out.println("Enter your guess: ");
+                int guess = scanner.nextInt();
+                if (guess < x) { System.out.println("x is greater!"); continue; }
+                if (guess > x) { System.out.println("x is smaller!"); continue; }
+                System.out.println("You were correct!");
+                break;
+            }
+            System.out.println("Want to play again ? (y/n)");
+            // Consume trailing newline
+            scanner.nextLine();
+            if (!scanner.nextLine().equalsIgnoreCase("y")) break;
         }
     }
 }
